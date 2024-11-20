5af418cf503ee5b2679d5a9635a74b5e382a01a4
EliannyBello <elibellomanzo@gmail.com>
2024-11-20 15:45:08 -0300

    init

---
+++ src/components/InvoiceAPP.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const InvoceApp = () => {
+
+    return (
+        <>
+        
+        <h1>Hola mundo</h1>
+        </>
+        
+
+     
+    )
+}
+
+export default InvoceApp;
---
+++ src/data/invoice.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const invoce = {
+    id: 10,
+    client: {
+        name: 'Pepe',
+        lastName: 'Doe',
+        address: {
+            country: 'USA',
+            city: 'Los Angeles',
+            street: 'One Street',
+            number: 12
+
+        }
+    },
+    company: {
+        name: 'New Egg',
+        fiscalNumber: 2549852,
+    },
+    items: [
+        {
+            product: 'table',
+            price: 50,
+            quantuty: 4,
+        },
+        {
+            product: 'mouse',
+            price: 20,
+            quantuty: 10,
+        },
+        {
+            product: 'mousePad',
+            price: 15,
+            quantuty: 5,
+        }
+    ],
+    
+}
